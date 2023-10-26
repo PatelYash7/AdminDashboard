e38455dd11901eb8943cbb95c20f2a318babc07d
PatelYash7 <yashpate0101@gmail.com>
2023-10-26 18:11:52 +0530

    EcommercePage Done

--- src/components/Charts/Sparkline.jsx
+++ src/components/Charts/Sparkline.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React from 'react';
+import { SparklineComponent, Inject, SparklineTooltip } from '@syncfusion/ej2-react-charts';
 
-const Sparkline = () => {
+const Sparkline = ({ currentColor, id, type, height, width, data, color }) => {
   return (
-    <div>Sparkline</div>
+    <div>
+      <SparklineComponent
+        id={id}
+        height={height}
+        width={width}
+        lineWidth={1}
+        valueType="Numeric"
+        fill={color}
+        border={{ color: currentColor, width: 2 }}
+        tooltipSettings={{
+          visible: true,
+
+          format: '${x} : data ${yval}',
+          trackLineSettings: {
+            visible: true,
+          },
+        }}
+        dataSource={data}
+        xName="x"
+        yName="yval"
+        type={type}
+
+      >
+        <Inject services={[SparklineTooltip]} />
+      </SparklineComponent>
+
+    </div>
   )
 }
 
--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { GoPrimitiveDot } from 'react-icons/go';
+import { GoDotFill } from 'react-icons/go';
 import { BsCurrencyDollar } from 'react-icons/bs';
-import { Stacked, Sparkline, Pie, Button } from '../components';
+import { Stacked, SparkLine, Pie } from '../components';
+import { Button } from '../components';
 import { useStateContext } from '../context/ContextProvider';
 import { earningData, medicalproBranding, recentTransactions, weeklyStats, dropdownData, SparklineAreaData, ecomPieChartData } from '../data/dummy';
 
@@ -16,7 +17,7 @@ const Ecommerce = () => {
               <p className="text-2xl">₹63,448,88</p>
             </div>
           </div >
-          
+
           <div className='mt-6'>
             <Button
               color='white'
@@ -29,7 +30,7 @@ const Ecommerce = () => {
         </div>
         {/* Earning data */}
         <div className='flex m-5 flex-wrap justify-bewteen gap-1 items-center'>
-        {earningData.map((item) => (
+          {earningData.map((item) => (
             <div key={item.title} className="bg-white h-44 dark:text-gray-200 dark:bg-secondary-dark-bg md:w-56  p-4 pt-9 rounded-2xl ">
               <button
                 type="button"
@@ -50,6 +51,94 @@ const Ecommerce = () => {
         </div>
       </div>
       {/* Revenue Updates */}
+      <div className='flex flex-wrap gap-10 justify-center'>
+        <div className='bg-white dark:text-gray-200 dark:bg-secondary-dark-bg m-3 p-4 rounded-2xl md:w-780'>
+          <div className='flex justify-between'>
+            <p className='font-semibold text-xl'>Revenue Update</p>
+            <div className='flex items-center gap-4'>
+              <p className='text-gray-600 flex items-center gap-2 hover:drop-shadow-xl'>
+                <span>
+                  <GoDotFill />
+                </span>
+                <span>Expense</span>
+              </p>
+              <p className='text-green-400 flex items-center gap-2 hover:drop-shadow-xl'>
+                <span>
+                  <GoDotFill />
+                </span>
+                <span>Budget</span>
+              </p>
+            </div>
+          </div>
+          {/* Charts of Revenue Section */}
+          <div className='mt-10 flex gap-10 flex-wrap justify-center'>
+            <div className='border-r-1 border-color m-4 pr-10'>
+              <div>
+                <div>
+                  <p>
+                    <span className="text-3xl font-semibold">$93,438</span>
+                    <span className="p-1.5 hover:drop-shadow-xl cursor-pointer rounded-full text-white bg-green-400 ml-3 text-xs">
+                      23%
+                    </span>
+                  </p>
+                  <p className="text-gray-500 mt-1">Budget</p>
+                </div>
+                <div className="mt-8">
+                  <p className="text-3xl font-semibold">$48,487</p>
+
+                  <p className="text-gray-500 mt-1">Expense</p>
+                </div>
+                <div className="mt-5">
+                  <SparkLine currentColor="blue" id="line-sparkLine" type="Line" height="80px" width="250px" data={SparklineAreaData} color="blue" />
+                </div>
+                <div className="mt-10">
+                  <Button
+                    color="white"
+                    bgColor="Blue"
+                    text="Download Report"
+                    borderRadius="10px"
+                  />
+                </div>
+              </div>
+            </div>
+            <div>
+              <Stacked width="320px" height="360px" />
+            </div>
+          </div>
+        </div>
+        <div>
+          <div
+            className=" rounded-2xl md:w-400 p-4 m-3"
+            style={{ backgroundColor: "blue" }}
+          >
+            <div className="flex justify-between items-center ">
+              <p className="font-semibold text-white text-2xl">Earnings</p>
+
+              <div>
+                <p className="text-2xl text-white font-semibold mt-8">$63,448.78</p>
+                <p className="text-gray-200">Monthly revenue</p>
+              </div>
+            </div>
+
+            <div className="mt-4">
+              <SparkLine currentColor="blue" id="column-sparkLine" height="100px" type="Column" data={SparklineAreaData} width="320" color="rgb(242, 252, 253)" />
+            </div>
+          </div>
+
+          <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg rounded-2xl md:w-400 p-8 m-3 flex justify-center items-center gap-10">
+            <div>
+              <p className="text-2xl font-semibold ">$43,246</p>
+              <p className="text-gray-400">Yearly sales</p>
+            </div>
+
+            <div className="w-40">
+              <Pie id="pie-chart" data={ecomPieChartData} legendVisiblity={false} height="160px" />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      
     </div>
   )
 }
